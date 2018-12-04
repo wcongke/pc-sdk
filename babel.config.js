753f58808aead1273c428b2abfa84fd7c7f53337
wcongke <wcongke@castles.com.cn>
2018-12-04 18:59:07 +0800

    fix: 修改babel配置

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,30 @@
 module.exports = {
   presets: [
-    ['env', {
-      modules: false,
-      targets: {
-        browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+    '@vue/app',
+    // '@babel/preset-stage-2',
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        targets: {
+          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+        }
       }
-    }],
-    'stage-2',
-    '@vue/app'
+    ]
   ],
-  plugins: ['transform-runtime'],
-  env: {
-    test: {
-      presets: ['env', 'stage-2'],
-      plugins: ['istanbul']
-    }
-  }
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    // Stage 2
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+
+    // Stage 3
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    '@babel/plugin-proposal-json-strings'
+  ]
 }
